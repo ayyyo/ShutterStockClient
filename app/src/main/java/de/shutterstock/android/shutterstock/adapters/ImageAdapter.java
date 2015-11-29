a0e96c05f8eeb1188fdfe6bbebafb0abd146db70
Emanuele Gringeri <emanuele.gringeri@erasys.de>
2015-11-29 21:25:06 +0100

    trying out Picasso instead of Fresco

--- app/src/main/java/de/shutterstock/android/shutterstock/adapters/ImageAdapter.java
+++ app/src/main/java/de/shutterstock/android/shutterstock/adapters/ImageAdapter.java
@@ -1,17 +1,30 @@
 package de.shutterstock.android.shutterstock.adapters;
 
-import android.net.Uri;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.ActivityOptionsCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import com.facebook.drawee.drawable.ScalingUtils;
+import com.facebook.drawee.generic.GenericDraweeHierarchy;
+import com.facebook.drawee.generic.GenericDraweeHierarchyBuilder;
 import com.facebook.drawee.view.SimpleDraweeView;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import de.shutterstock.android.shutterstock.R;
+import de.shutterstock.android.shutterstock.activities.PictureDetailsActivity;
 import de.shutterstock.android.shutterstock.content.model.Image;
 
 /**
@@ -19,19 +32,53 @@ import de.shutterstock.android.shutterstock.content.model.Image;
  */
 public class ImageAdapter extends RecyclerView.Adapter<ImageAdapter.ImageViewHolder> {
 
-    public static class ImageViewHolder extends RecyclerView.ViewHolder {
+    public static class ImageViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
-        private final SimpleDraweeView mImageView;
+        private final SimpleDraweeView mSimpleDraweeView;
+        private final ImageView mImageView;
         private Image mImage;
+
         public ImageViewHolder(View itemView) {
             super(itemView);
-            mImageView = (SimpleDraweeView) itemView.findViewById(R.id.image);
+            final Context context = itemView.getContext();
+            GenericDraweeHierarchyBuilder builder =
+                    new GenericDraweeHierarchyBuilder(context.getResources());
+            List<Drawable> drawables = new ArrayList<>();
+            drawables.add(new ColorDrawable(ContextCompat.getColor(context, R.color.accent)));
+            drawables.add(new ColorDrawable(ContextCompat.getColor(context, R.color.accent_material_light)));
+            GenericDraweeHierarchy hierarchy = builder
+                    .setFadeDuration(300)
+                    .setPlaceholderImage(new ColorDrawable(ContextCompat.getColor(context, R.color.accent_material_light)))
+                    .setBackgrounds(drawables)
+                    .setProgressBarImage(ContextCompat.getDrawable(context, R.mipmap.ic_launcher))
+                    .setActualImageScaleType(ScalingUtils.ScaleType.CENTER_CROP)
+                    .build();
+            mSimpleDraweeView = (SimpleDraweeView) itemView.findViewById(R.id.image2);
+            // mSimpleDraweeView.setHierarchy(hierarchy);
+            mImageView = (ImageView) itemView.findViewById(R.id.image);
+            mImageView.setOnClickListener(this);
         }
 
         public void updateImage(Image image) {
             mImage = image;
-            mImageView.setImageURI(Uri.parse(image.assets.preview.url));
-            mImageView.setAspectRatio(image.aspect);
+            //mImageView.setImageURI(Uri.parse(image.assets.preview.url));
+            //mImageView.setAspectRatio(image.aspect);
+            Picasso.with(mImageView.getContext())
+                    .load(image.assets.preview.url)
+                    .into(mImageView);
+        }
+
+        @Override
+        public void onClick(View v) {
+            if (!(v.getContext() instanceof Activity)) {
+                return;
+            }
+            final Activity activity = (Activity) v.getContext();
+            Intent intent = new Intent(activity, PictureDetailsActivity.class);
+            intent.putExtra(PictureDetailsActivity.IMAGE_KEY, mImage);
+            ActivityOptionsCompat options = ActivityOptionsCompat.
+                    makeSceneTransitionAnimation(activity, mImageView, "profile");
+            ActivityCompat.startActivity(activity, intent, options.toBundle());
         }
     }
 
